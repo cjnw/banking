0fb0a071fd2c2bc94878558d5a0c290344d64713
CJ <cj.cn84@gmail.com>
2024-08-05 15:52:22 -0400

    Add sidebar

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,4 +1,7 @@
+
 import React from 'react'
+import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = ({
     accounts = [], totalBanks, totalCurrentBalance
@@ -6,13 +9,23 @@ const TotalBalanceBox = ({
     return (
         <section className='total-balance'>
             <div className='total-balance-chart'>
-                {/* DoughnutChart */}
+                <DoughnutChart accounts={accounts} />
             </div>
 
             <div className='flex flex-col gap-6'>
                 <h2 className='header-2'>
-                    Bank ccounts: {totalBanks}
+                    Bank Accounts: {totalBanks}
                 </h2>
+                <div className="flex flex-col gap-2">
+                    <p className='total-balance-label'>
+                        Total Current Balance
+                    </p>
+
+                    <div className='total-balance-amount flex-center gap-2'>
+                        <AnimatedCounter amount=
+                            {totalCurrentBalance} />
+                    </div>
+                </div>
             </div>
         </section>
     )
